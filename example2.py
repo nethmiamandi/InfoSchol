9217c2035090496e9cdb1afefba9670f11b12e6a
nethmiamandi <nethmiwaththegama0@gmail.com>
2025-04-20 23:58:26 +0530

    added

---
+++ example2.py
@@ -0,0 +1,37 @@
+
+    
+character_name = "john"
+character_age = "35"
+print("there was a man his name was named " + character_name + ",")
+print("his age was " + character_age + ".")
+
+print("Amandi\nAcademy")
+phrase = "Amandi Academy"
+print(phrase.index("A"))
+print(phrase.replace("Amandi","Elephant"))
+
+
+print(pow(4, 6))
+
+name = input("Input your name:")
+a3ge = input("Input your age:")
+print("hi " + name + "! your are " + age +"")
+
+num1 = input("Enter a number:")
+num2 = input("Enter a another number:")
+result = float(num1) + float(num2)
+print(result)
+
+colour = input("Enter a colour:")
+Plural_noun = input("Enter a plural noun:")
+Celebrity_name = input("Enter a Celebrity name:")
+
+print("roses are " + colour )
+print(Plural_noun + " are blue")
+print("I love " + Celebrity_name )
+
+def say_hi(name):
+    print("Hello " + name)
+
+say_hi("mike")
+say_hi("nike")
